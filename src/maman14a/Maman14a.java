461679864d4fec3735227b79c80a006aa1d85b97
elira <elira@DESKTOP-40OR9PA>
2017-12-26 23:50:01 +0200

    First commit

---
+++ src/maman14a/Maman14a.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package maman14a;
+
+/**
+ *
+ * @author elira
+ */
+public class Maman14a {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
